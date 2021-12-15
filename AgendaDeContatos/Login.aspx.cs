db946b89c60911e034fc49cfa4fee75a5a803692
Leonardo <leosilvarocha.lsr@gmail.com>
2021-12-15 15:24:30 -0300

    Implementação da tela de login.

---
+++ AgendaDeContatos/Login.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AgendaDeContatos
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnEntrar_Click(object sender, EventArgs e)
+        {
+            String email = txtEmail.Text;
+            String senha = txtSenha.Text;
+
+            SqlConnection conexao = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+            try
+            {
+                conexao.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conexao;
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.CommandText = "SELECT * FROM Usuario WHERE email = @email and senha = @senha";
+                cmd.Parameters.AddWithValue("@email", txtEmail.Text);
+                cmd.Parameters.AddWithValue("@senha", txtSenha.Text);
+              
+                SqlDataReader registro = cmd.ExecuteReader();
+
+                if (registro.HasRows)
+                {
+                    lblMsg.Text = "Logado com secesso! ";
+                }
+                else
+                {
+                    lblMsg.Text = "Erro ao logar";
+                }
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+    }
+}
